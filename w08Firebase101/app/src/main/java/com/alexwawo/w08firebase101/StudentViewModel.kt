ddbadb51aaa96ebee3ec6cd4eadb45e56138676b
zerox <zeroxleashgaming@gmail.com>
2025-04-27 13:02:53 +0700

    bagian 1 cp 2

--- w08Firebase101/app/src/main/java/com/alexwawo/w08firebase101/StudentViewModel.kt
+++ w08Firebase101/app/src/main/java/com/alexwawo/w08firebase101/StudentViewModel.kt
@@ -56,3 +56,30 @@ class StudentViewModel : ViewModel() {
     }
 }
 
+fun updateStudent(student: Student) {
+    val data = mapOf(
+        "id" to student.id,
+        "name" to student.name,
+        "program" to student.program
+    )
+    db.collection("students").document(student.docId)
+        .set(data)
+        .addOnSuccessListener { fetchStudents() }
+        .addOnFailureListener { e -> Log.w("Firestore", "Error updating
+                document", e) }
+        }
+    fun deleteStudent(student: Student) {
+        db.collection("students").document(student.docId)
+            .delete()
+            .addOnSuccessListener { fetchStudents() }
+            .addOnFailureListener { e -> Log.w("Firestore", "Error deleting
+                    document", e) }
+            }
+        private fun fetchStudents() {
+            ...
+            val docId = document.id
+            list.add(Student(id, name, program, docId))
+            ...
+        }
+
+
